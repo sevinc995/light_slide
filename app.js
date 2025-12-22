ba9da69ffec251ca2dedefdd56141f04d2d09fca
sevinc995 <sevincseferova25@gmail.com>
2025-12-22 09:06:40 +0000

    commited

--- app.js
+++ app.js
@@ -2,25 +2,7 @@
 $(document).ready(function () {
 
 });
-// var modal = document.getElementById("myModal");
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
 
-// var images = document.getElementsByClassName("myImg");
-
-// for (let i = 0; i < images.length; i++) {
-//     images[i].onclick = function () {
-//         modal.style.display = "block";
-//         modalImg.src = this.src;
-//         captionText.innerHTML = this.alt;
-//     };
-// }
-
-// var span = document.getElementsByClassName("close")[0];
-
-// span.onclick = function () {
-//     modal.style.display = "none";
-// };
 
 var modal = document.getElementById("myModal");
 var modalImg = document.getElementById("img01");
